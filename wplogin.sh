d0ebacd80851bda3d4c2f573bf5552c4c75e743b
Daniel Yount <factorf2@yahoo.com>
2020-05-13 00:27:18 -0500

    edit
    edit

--- wplogin.sh
+++ wplogin.sh
@@ -1,2 +1,2 @@
-url -c cookie.txt -F log=admin -F pwd='password' https://www.example.com/wp-login.php
+curl -c cookie.txt -F log=admin -F pwd='password' https://www.example.com/wp-login.php
 curl -o output.html --cookie cookie.txt https://www.exmaple.com/wp-json/specialplugin/v1/users/10
